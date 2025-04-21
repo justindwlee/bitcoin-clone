34ecc086dc0b0d818cd0130b21efc63f83b21a6d
Justin Lee <dwlee1399@gmail.com>
2025-04-21 22:16:06 +0900

    Lecture ~5.6

--- main.go
+++ main.go
@@ -1,19 +1,9 @@
 package main
 
-import (
-	"fmt"
+import "github.com/justindwlee/bitcoinClone/explorer"
 
-	"github.com/justindwlee/bitcoinClone/blockchain"
-)
 
 
 func main(){
-	chain := blockchain.GetBlockchain()
-	chain.AddBlock("Second Block")
-	chain.AddBlock("Third Block")
-	for _, block := range chain.AllBlocks() {
-		fmt.Printf("Data: %s\n", block.Data)
-		fmt.Printf("Hash: %s\n", block.Hash)
-		fmt.Printf("PrevHash: %s\n", block.PrevHash)
-	}
+	explorer.Start()
 }
